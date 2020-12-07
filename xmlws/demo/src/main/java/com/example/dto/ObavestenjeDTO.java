3c7b0306dc78a35ac7bffed1522d5f4d859d5553
Srbislav98 <srbislav30111998@gmail.com>
2020-12-07 18:12:27 +0100

    inicijalizacija projekta

---
+++ xmlws/demo/src/main/java/com/example/dto/ObavestenjeDTO.java
@@ -0,0 +1,27 @@
+package com.example.dto;
+
+public class ObavestenjeDTO {
+
+	
+	private String text;
+
+	
+	public ObavestenjeDTO() {
+		super();
+	}
+
+	public ObavestenjeDTO(String text) {
+		super();
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+
+}
