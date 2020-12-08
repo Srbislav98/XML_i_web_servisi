543936fb9ee1128f7498bfd52923df23d0d50924
JovanCorilic <kssbc39@gmail.com>
2020-12-08 09:31:38 +0100

    Uradjeno obavestenje kako ja mislim da treba

---
+++ Parser/src/parser/test/MarshallingObavestenjeCir.java
@@ -0,0 +1,35 @@
+package parser.test;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import rs.ac.uns.ftn.obavestenjecir.Obavestenje;
+
+public class MarshallingObavestenjeCir {
+	
+	public void test() throws Exception{
+		try {
+			System.out.println("[INFO] Obavestenjecir.\n");
+			JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.obavestenjecir");
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			Obavestenje obavestenje = (Obavestenje)unmarshaller.unmarshal(new File("./data/obavestenjecir.xml"));
+			
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.marshal(obavestenje, System.out);
+		}
+		catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		MarshallingObavestenjeCir test = new MarshallingObavestenjeCir();
+		test.test();
+	}
+
+}
