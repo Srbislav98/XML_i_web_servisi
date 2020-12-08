dba000254e6b2053c308a1ed98dd66ade922f574
sw792017 <52822584+sw792017@users.noreply.github.com>
2020-12-08 10:41:52 +0100

    marshall za zahtevcir

---
+++ Parser/src/rs/ac/uns/ftn/zahtevcirMarshalling/MarshallingZahtevcir.java
@@ -0,0 +1,34 @@
+package rs.ac.uns.ftn.zahtevcirMarshalling;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import rs.ac.uns.ftn.resenja.Resenje;
+import rs.ac.uns.ftn.resenja.test.MarshallingPrvoResenje;
+
+public class MarshallingZahtevcir {
+	public void test() throws Exception{
+		try {
+			System.out.println("[INFO] Resenja: JAXB marshalling.\n");
+			JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.zahtevcir");
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			Resenje resenje = (Resenje)unmarshaller.unmarshal(new File("./data/zahtevcir.xml"));
+			
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.marshal(resenje, System.out);
+		}
+		catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		MarshallingPrvoResenje test = new MarshallingPrvoResenje();
+		test.test();
+	}
+}
