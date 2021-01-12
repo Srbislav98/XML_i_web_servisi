3811a791674d5ace0b94bc7c7f7567f491e10361
djordjeognjenovic97 <djordjeognjenovic997@gmail.com>
2021-01-12 09:07:08 +0100

    Uradjeno ucitavanje i upisivanje (exist i rdf) za resenja i ispravljene sitne greske

--- projekat/src/main/java/com/tim15/projekat/controller/ResenjaController.java
+++ projekat/src/main/java/com/tim15/projekat/controller/ResenjaController.java
@@ -1,7 +1,7 @@
 package com.tim15.projekat.controller;
 
 import com.tim15.projekat.dto.TestDTO;
-import com.tim15.projekat.service.ZahtevicirService;
+import com.tim15.projekat.service.ResenjaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,26 +10,26 @@ import org.springframework.web.bind.annotation.*;
 import org.w3c.dom.Document;
 
 @RestController
-@RequestMapping(value = "api/zahtevi", produces = MediaType.APPLICATION_JSON_VALUE)
-public class ZahtevicirController {
+@RequestMapping(value = "api/resenja", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ResenjaController {
     @Autowired
-    private ZahtevicirService zahtevicirService;
+    private ResenjaService resenjaService;
 
     @PostMapping(value = "/addText", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> addZahtevText(@RequestBody TestDTO testDTO) throws Exception {
-        zahtevicirService.addZahtevFromText(testDTO.getText());
+    public ResponseEntity<String> addResenjeText(@RequestBody TestDTO testDTO) throws Exception {
+        resenjaService.addResenjeFromText(testDTO.getText());
         return new ResponseEntity<String>("Done", HttpStatus.OK);
     }
 
     @PostMapping(value = "/addFile", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> addZahtevFile(@RequestBody String path) throws Exception {
-        zahtevicirService.addZahtevFromFile(path);
+    public ResponseEntity<String> addResenjeFile(@RequestBody String path) throws Exception {
+        resenjaService.addResenjeFromFile(path);
         return new ResponseEntity<String>("Done", HttpStatus.OK);
     }
 
     @GetMapping(value = "/getDocument/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Document> getZahtevDocument(@PathVariable String id) throws Exception {
-        Document document = zahtevicirService.getZahtevDocument(id);
+    public ResponseEntity<Document> getResenjeDocument(@PathVariable String id) throws Exception {
+        Document document = resenjaService.getResenjeDocument(id);
         if(document == null) {
             return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
--- projekat/src/main/java/com/tim15/projekat/repository/ResenjaRepository.java
+++ projekat/src/main/java/com/tim15/projekat/repository/ResenjaRepository.java
@@ -7,21 +7,21 @@ import org.w3c.dom.Document;
 import org.xmldb.api.modules.XMLResource;
 
 @Repository
-public class ZahtevicirRepository {
+public class ResenjaRepository {
     @Autowired
     ExistManager existManager;
 
-    private String collectionId = "/existdb/zahtevi";
+    private String collectionId = "/existdb/resenja";
 
-    public void saveZahtevFromText(String text, String id) throws Exception {
+    public void saveResenjeFromText(String text, String id) throws Exception {
         existManager.storeFromText(collectionId, id , text);
     }
 
-    public void saveZahtevFromFile(String path, String id) throws Exception {
+    public void saveResenjeFromFile(String path, String id) throws Exception {
         existManager.store(collectionId, id, path);
     }
 
-    public Document findZahtevById(String id) throws Exception {
+    public Document findResenjeById(String id) throws Exception {
         XMLResource xmlResource = existManager.load(collectionId, id);
         Document document = (Document) xmlResource.getContentAsDOM();
         return document;
