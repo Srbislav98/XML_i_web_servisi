a74f9782e2ed0bbe06f14664b3af760e5f43da10
JovanCorilic <kssbc39@gmail.com>
2021-02-26 09:02:31 +0100

    dodati modeli i uzeti fajlovi iz projekta 'projekat'

---
+++ poverenik/src/main/java/com/poverenik/poverenik/service/ObavestenjecirService.java
@@ -0,0 +1,49 @@
+package com.poverenik.poverenik.service;
+
+import com.poverenik.poverenik.jaxb.JaxbParser;
+import com.poverenik.poverenik.jenafuseki.FusekiWriterExample;
+import com.poverenik.poverenik.jenafuseki.MetadataExtractor;
+import com.poverenik.poverenik.model.obavestenjecir.Obavestenje;
+import com.poverenik.poverenik.repository.ObavestenjecirRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+@Service
+public class ObavestenjecirService {
+    @Autowired
+    private ObavestenjecirRepository obavestenjecirRepository;
+
+    private JaxbParser jaxbParser;
+    private MetadataExtractor metadataExtractor;
+
+    public ObavestenjecirService() throws IOException, SAXException {
+        jaxbParser = new JaxbParser();
+        metadataExtractor = new MetadataExtractor();
+    }
+
+    public void addObavestenjeFromText(String text) throws Exception {
+        Obavestenje obavestenje = jaxbParser.unmarshall(Obavestenje.class, text);
+        String docId = obavestenje.getNaziv();
+        obavestenjecirRepository.saveObavestenjeFromText(text, docId);
+        metadataExtractor.extractMetadata(text);
+        FusekiWriterExample.saveRDF();
+    }
+
+    public void addObavestenjeFromFile(String path) throws Exception {
+        Obavestenje obavestenje = jaxbParser.unmarshallFile(Obavestenje.class, path);
+        String docId = obavestenje.getNaziv();
+        obavestenjecirRepository.saveObavestenjeFromFile(path, docId);
+        String text = jaxbParser.marshallString(Obavestenje.class, obavestenje);
+        metadataExtractor.extractMetadata(text);
+        FusekiWriterExample.saveRDF();
+    }
+
+    public Document getObavestenjeDocument(String docId) throws Exception {
+        Document doc = obavestenjecirRepository.findObavestenjeById(docId);
+        return doc;
+    }
+}
